d84ae1a500fa2bb16dde5a19c957d28db4d69968
andres alcocer <devandres.tech@gmail.com>
2022-06-24 11:24:31 -0400

    setup nodemon

---
+++ token-auth/server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const corsOption = {
+  origin: 'http://localhost:8000',
+}
+
+app.use(cors(corsOption))
+
+// parse requests of content-type: application/json
+app.use(express.json())
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to token based auth' })
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
